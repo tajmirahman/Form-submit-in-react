b90c4aa2c86538d5ff0ecd4bfa430543705014b2
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-02-25 16:08:36 +0600

    password valided done

--- form-submit/src/Components/StateForm/StateForm.jsx
+++ form-submit/src/Components/StateForm/StateForm.jsx
@@ -5,27 +5,47 @@ const StateForm = () => {
 
     const [email, setEmail]= useState();
     const [name, setName]= useState();
+    const [password, setPassword]= useState();
+    const [error, setError]= useState('');
 
     const handelSubmitForm=e=>{
         e.preventDefault();
-        console.log(email);
+
+        if(password.length <=6){
+            setError('password must be need 6 character!')
+        }
+        else{
+            setError('');
+            console.log(email, name, password);
+        }
+
     }
 
-    const 
+    const handleNameChange=e=>{
+        setName(e.target.value)
+    }
 
     const handleEmailChange=(e)=>{
         setEmail(e.target.value)
+    }
+
+    const handlePassword=e=>{
+        setPassword(e.target.value);
     }
     
 
     return (
         <div className="bg-slate-300 p-3">
             <form onSubmit={handelSubmitForm}>
-                <input  type="text" name="text"  placeholder="name"/><br /><br />
+
+                <input onChange={handleNameChange}  type="text" name="text"  placeholder="name"/><br /><br />
 
                 <input onChange={handleEmailChange} type="email"  name="email" placeholder="email"/><br /><br />
 
-                <input type="password" name="" id="" /><br />
+                <input onChange={handlePassword} type="password" name="" id="" /><br />
+                {
+                    error && <p className="text-rose-500">{error}</p>
+                }
 
                 <input type="submit" />
             </form>
