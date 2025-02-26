2e733cbc4008d5fc517d023709f20951339fa7ea
tajmirahman <127497419+tajmirahman@users.noreply.github.com>
2025-02-26 11:14:40 +0600

    Update App.jsx

--- form-submit/src/App.jsx
+++ form-submit/src/App.jsx
@@ -1,14 +1,25 @@
 
 import './App.css'
-import HookForm from './Components/HookForm/HookForm'
+// import HookForm from './Components/HookForm/HookForm'
 // import RefForm from './Components/RefForm/RefForm'
 // import FormSubmit from './Components/FormSubmit/FormSubmit'
 // import StateForm from './Components/StateForm/StateForm'
+import ReuseableForm from './Components/ReuseableForm/ReuseableForm'
 
 function App() {
 
 
   return (
+
+    const handleSignUp=data=>{
+
+    console.log(data)
+  }
+
+  const handleUpdate=data=>{
+    console.log(data)
+  }
+    
     <>
     
       {/* <FormSubmit></FormSubmit> */}
@@ -16,7 +27,19 @@ function App() {
 
       {/* <RefForm></RefForm> */}
 
-      <HookForm></HookForm>
+{/*       <HookForm></HookForm> */}
+
+
+  <ReuseableForm
+        formTitle={'Sign Up'}
+        handleSubmitForm={handleSignUp}
+      ></ReuseableForm>
+
+      <ReuseableForm
+        formTitle={'Update Profile'}
+        submitBtn={'Update'}
+        handleSubmitForm={handleUpdate}
+      ></ReuseableForm>
    
       
       
